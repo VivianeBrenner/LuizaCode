82912346b62c13dd00d4572325cb00b99315e1db
Ana Higo <anahigo.arq@gmail.com>
2021-09-04 16:28:22 -0300

    Correçao Server

--- server.ts
+++ server.ts
@@ -4,10 +4,9 @@ import routes from './routes'
 
 const app = express()
 const port = 3000
+app.use(express.json())
 
 app.use(routes) 
-
-app.use(express.json())
 
 app.listen(port, () => {
     console.log(`Servidor inicializado na porta ${port}`)
--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -4,12 +4,13 @@ import { UsersRepository } from '../repositories/UsersRepository'
 
 export default {
   async createUsersController( req: Request, res: Response) {
-  console.log(req)
+  console.log(req.body)
   const { name, email, password } = req.body
   
   let user = { name, email, password }
   console.log(user)
   const repository = getCustomRepository(UsersRepository)
+  
   user = await repository.save(user)
 
   return res.status(201).json({ user })
